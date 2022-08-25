7b30eb7333f2bba83985d5055f5c5e901123fcaa
ankur <ankur25590655@gmail.com>
2022-08-25 17:34:02 +0530

    "EJB Server fetch"

--- todo.js
+++ todo.js
@@ -6,71 +6,50 @@ const request = require("request");
 
 
 
-const app =  express();
+const app = express();
+
+
+var items = [];
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
 
-    var today = new Date();
-    var day = today.getDay();
-    var dateIs = today.toISOString().slice(0,10);
-    // var daylight = {
-    //     whichdate:dateIs,
-    //     whichday:day
-    // };
-
-    // var jsonDate = JSON.stringify(daylight);
-    
-    // console.log(jsonDate);
-
-   
-
-    switch (day) {
-        case 0:
-            res.render("list",{userday: "Sunday",userdate:dateIsteIs});
-            
-            break;
-
-        case 1:
-            res.render("list",{userday: "Monday",userdate:dateIs});
-            
-            
-            break;
-
-        case 2:
-            res.render("list",{userday:"Tuesday",userdate:dateIs});
-            
-            break;
-        case 3:
-            res.render("list",{userday: "Wednesday",userdate:dateIs});
-            
-            break;
-        case 4:
-            res.render("list",{userday: "Thursday",userdate:dateIs});
-            
-            break;
-        case 5:
-            res.render("list",{userday: "Friday",userdate:dateIs});
-            
-            break;
-        case 6:
-            res.render("list",{userday: "Saturday",userdate:dateIs});
-            
-            break;
-        default:
-            res.render("list",{userday:"Wrong day"});
-            
-            break;
-    }
+  var today = new Date();
+  var options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric"
+  };
+  var day = today.toLocaleDateString("en-Us", options);
+  res.render("list", {
+    userday: day,
+    newitem: items
+  });
 
 
 
 });
 
-app.listen(3000,()=>{
-    console.log("server running at 3000");
+app.post("/", (req, res) => {
+
+  var item = req.body.Additem;
+
+  items.push(item);
+
+  res.redirect("/");
+
+
+});
+
+
+app.listen(3000, () => {
+  console.log("server running at 3000");
 });
